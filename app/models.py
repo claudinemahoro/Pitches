96acf0cc9c8caeb306ba2ee0d19d7734ebf52950
claudinemahoro <claudinemah3@gmail.com>
2020-12-02 15:21:22 +0200

    creating comments class in models.py

--- app/models.py
+++ app/models.py
@@ -121,4 +121,22 @@ class Downvote(db.Model):
     def __repr__(self):
         return f"{self.user_id}:{self.pitch_id}"
 
+   
+class Comment(db.Model):
+    __tablename__ = 'comments'
 
+    id = db.Column(db.Integer,primary_key=True)
+    comment = db.Column(db.Text(),nullable = False)
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+    pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
+
+    def save_comments(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_comments(cls,pitch_id):
+        comments = Comment.query.filter_by(pitch_id=pitch_id).all()
+
+    def __repr__(self):
+        return f"comment:{self.comment}"
