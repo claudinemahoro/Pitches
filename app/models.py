0132204aa7274f556e65b04bd20649a5ecb7fb9b
claudinemahoro <claudinemah3@gmail.com>
2020-12-02 15:20:49 +0200

    creating downvote class in models.py

--- app/models.py
+++ app/models.py
@@ -101,3 +101,24 @@ class Upvote(db.Model):
         return f"{self.user_id}:{self.pitch_id}"
 
 
+class Downvote(db.Model):
+    __tablename__ = 'downvotes'
+
+    id =  db.Column(db.Integer,primary_key=True)
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+    pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
+    
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_downvotes(cls,id):
+        downvotes = Downvote.query.filter_by(pitch_id = id).all()
+        return downvotes
+
+    def __repr__(self):
+        return f"{self.user_id}:{self.pitch_id}"
+
+
